21708a56bd248bb4961dbd50450bab5c30d31699
Lukeveille <luke@lukeleveille.com>
2018-11-21 08:18:47 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default Hello;
